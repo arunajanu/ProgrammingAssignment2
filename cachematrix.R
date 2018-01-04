9fa5e520c8bdfc9c124fb9386f079c76423c6e8c
arunajanu <35108645+arunajanu@users.noreply.github.com>
2018-01-04 11:24:26 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,59 @@
 ## functions do
 
 ## Write a short comment describing this function
+# this function gets matrix as an input, set the value of matrix, get the value of matrix, set the inverse matrix and get the inverse matrix.Matrix object can cache its own object
 
 makeCacheMatrix <- function(x = matrix()) {
+	invMatrix <- NULL
+#set the value of the matrix
+     setMatrix <- function(y) {
+     	x <<- y
+     	invMatrix <<- NULL
+     }
+   
+   getMatrix <- function() x
+   setInverse <- function(inverse) invMatrix <<- inverse
+   getInverse <- function() invMatrix
+   list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+## this function takes output of previous matrix as an input and checks inverse matrix has any value in it or not. In case inverse matrix from makeCacheMatrix is empty, it gets the original matrix data from and set the invertible matrix by solve function.
 cacheSolve <- function(x, ...) {
+#get value of invertible matrix from makeCacheMatrix function
+         invMatrix <- x$getInverse()
+        if(!is.null(invMatrix)) {
+        	message("Getting cached Invertible Matrix")
+        	return(invMatrix)
+        }
+  
+ #if value of the invertible matrix is NULL then
+       MatrixData <- x$getMatrix()
+       invMatrix <- solve(MatrixData,...)
+       x$setInverse(invMatrix)
+       return(invMatrix)
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Test 1
+TestMatrix <- matrix(1:4,2,2)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+#Test3
+TestMatrix <- matrix(1:9,3,3)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
